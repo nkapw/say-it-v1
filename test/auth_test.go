766fa661330d3c015d6c41bfd74250e34521e97e
Imam Ahmad Fahrezi <imamfahrezi24@gmail.com>
2023-12-06 00:27:06 +0700

    test auth

--- test/auth_test.go
+++ test/auth_test.go
@@ -1,9 +1,12 @@
 package test
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http/httptest"
+	"say-it/handlers"
 	"strings"
 	"testing"
 )
@@ -23,5 +26,24 @@ func TestRegister(t *testing.T) {
 	//body, err := io.ReadAll(res.Body)
 	//assert.Nil(t, err)
 	//assert.Equal(t, "Hello imam", string(body))
+
+}
+
+func TestUpdate(t *testing.T) {
+
+	request := httptest.NewRequest("POST", "/user/update", nil)
+
+	request.Header.Set("Au", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDE3OTMwMjcsImlhdCI6MTcwMTc4OTQyNywic3ViIjoiMiJ9.sjtuQ2Tj7RZ4U06DZ5sTL3UzuLp-WZfy7kJn1GsvAsA")
+
+	recorder := httptest.NewRecorder()
+
+	handlers.UpdateCurrentUserHandler(recorder, request)
+
+	response := recorder.Result()
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
 
 }
