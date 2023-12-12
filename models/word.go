68ac1a39400dfa22cbb8e5c86ba547535135d586
Imam A. Fahrezi <48815824+nkapw@users.noreply.github.com>
2023-12-12 17:46:00 +0700

    Update word.go

--- models/word.go
+++ models/word.go
@@ -1,1 +1,17 @@
 package models
+
+// models/word.go
+package models
+
+type Word struct {
+	ID    int    `json:"id"`
+	WordTxt  string `json:"word_txt"`
+	VoiceUrl string `json:"voice_url"`
+}
+
+
+  type WordDetail struct {
+	ID          int    `json:"id"`
+	WordID      int    `json:"word_id"`
+	Description string `json:"description"`
+}
